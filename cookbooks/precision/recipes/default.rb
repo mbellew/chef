ed4a5aa2039492f84b7d6b2059fc85cdda0ea3c9
mbellew <matthewb@labkey.com>
2017-04-06 13:30:51 -0700

    some packages

---
+++ cookbooks/precision/recipes/default.rb
@@ -0,0 +1,17 @@
+package 'awscli'
+package 'chromium-browser'
+package 'direnv'
+package 'docker.io'
+package 'r-base'
+package 'r-base-core'
+package 'lxd'
+package 'python3'
+package 'tree'
+
+package 'account-plugin-facebook' do
+  action :remove
+end
+
+package 'account-plugin-flickr' do
+  action :remove
+end
