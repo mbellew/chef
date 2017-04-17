578646890d5a72cb5f3ee66e45218c6883f939a7
mbellew <matthewb@labkey.com>
2017-04-16 20:59:14 -0700

    gems

---
+++ cookbooks/precision/metadata.rb
@@ -0,0 +1,9 @@
+maintainer        'me'
+maintainer_email  'me'
+license           'Apache 2.0'
+description       'Add Description here '
+name              'precision'
+version           '0.0.0'
+
+depends 'apt'
+depends 'build-essential'
